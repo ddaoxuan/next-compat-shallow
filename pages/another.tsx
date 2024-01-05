860586ef83f754b82c561d6f51b792ec6332cd15
ddaoxuan <dawid.daoxuan.uk@gmail.com>
2024-01-05 16:20:13 +0100

    repro broken shallow

--- pages/another.tsx
+++ pages/another.tsx
@@ -1,16 +1,16 @@
 import { useRouter } from "next/compat/router";
 
-export default function Home({ a }: { a: number }) {
+export default function Another({ a }: { a: number }) {
   const router = useRouter();
 
   return (
     <main>
-      <h1>Home - {a}</h1>
+      <h1>Another - {a}</h1>
       <button
         className="bg-gray-800 rounded my-4 p-4"
-        onClick={() => router?.push("/another", undefined, { shallow: true })}
+        onClick={() => router?.push("/", undefined, { shallow: true })}
       >
-        go to another page
+        go home page
       </button>
     </main>
   );
@@ -20,7 +20,7 @@ export const getStaticProps = () => {
   console.log("gsp running");
   return {
     props: {
-      a: 10,
+      a: 20,
     },
   };
 };
